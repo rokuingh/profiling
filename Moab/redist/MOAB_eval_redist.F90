dfbb353bdaef2cc3f052baebba29807b38020233
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-05-27 18:35:34 -0700

    modification to distgrids, add trace

--- Moab/redist/MOAB_eval_redist.F90
+++ Moab/redist/MOAB_eval_redist.F90
@@ -77,7 +77,7 @@ program MOAB_eval_redist
   ! Regridding using ESMF native Mesh
   call profile_mesh_redist(srcfile, moab=.false., rc=localrc)
    if (localrc /=ESMF_SUCCESS) then
-     write(*,*) "ERROR IN REGRIDDING SUBROUTINE!"
+     write(*,*) "ERROR IN REDIST SUBROUTINE!"
      stop
   endif
 
@@ -118,11 +118,17 @@ program MOAB_eval_redist
   integer :: localrc
   character(12) :: NM
   type(ESMF_VM) :: vm
-  type(ESMF_Mesh) :: srcMesh
-  type(ESMF_DistGrid) :: distgrid1, distgrid2
+  type(ESMF_Mesh) :: srcMesh, redistMesh
+  type(ESMF_DistGrid) :: distgrid1, distgrid2, distgrid3
 
-  ! integer, parameter :: numNode = 1639680
-  integer, parameter :: numNode = 6480
+  integer :: i
+  integer, allocatable :: ec(:)
+  integer, allocatable :: sil(:)
+  integer, allocatable :: asil(:)
+
+  ! grid sizes are hardcoded, just easier this way
+  ! integer, parameter :: numNode = 1639680  ! ll1280x1280_grid.esmf.nc
+  integer, parameter :: numNode = 6480 ! ll80x80_grid.esmf.nc
   integer :: minI, maxI, nn
 
   ! result code
@@ -159,39 +165,72 @@ program MOAB_eval_redist
   maxI = (localPet+1)*nn
   if (localPet == petCount-1) maxI = numNode
 
-  distgrid1 = ESMF_DistGridCreate(minIndex=(/minI/), maxIndex=(/maxI/), rc=localrc)
+  allocate(asil(maxI-minI+1))
+  do i=1,maxI-minI+1
+    asil(i) = minI + i - 1
+  enddo
+
+  distgrid1 = ESMF_DistGridCreate(arbSeqIndexList=asil, rc=localrc)
   if (localrc /=ESMF_SUCCESS) then
     rc=ESMF_FAILURE
     return
   endif
 
-print *, localPet, "# distgrid1 minI = ", minI, " maxI = ", maxI
+  print *, localPet, "# distgrid1 minI = ", minval(asil), " maxI = ", maxval(asil)
+  deallocate(asil)
 
   minI = (petCount - localPet - 1)*nn + 1
   maxI = (petCount - localPet)*nn
   if (localPet == 0) maxI = numNode
 
-  distgrid2 = ESMF_DistGridCreate(minIndex=(/minI/), maxIndex=(/maxI/), rc=localrc)
+  allocate(asil(maxI-minI+1))
+  do i=1,maxI-minI+1
+    asil(i) = minI + i - 1
+  enddo
+
+  distgrid2 = ESMF_DistGridCreate(arbSeqIndexList=asil, rc=localrc)
   if (localrc /=ESMF_SUCCESS) then
     rc=ESMF_FAILURE
     return
   endif
 
-print *, localPet, "# distgrid2 minI = ", minI, " maxI = ", maxI
+  print *, localPet, "# distgrid2 minI = ", minval(asil), " maxI = ", maxval(asil)
+  deallocate(asil)
 
-
+#define profile_mesh_create
 #ifdef profile_mesh_create
   call ESMF_TraceRegionEnter(NM//" Create")
   call ESMF_VMLogMemInfo("before "//NM//" create")
 #endif
 
- !!!! Setup source mesh !!!!
- srcMesh=ESMF_MeshCreate(filename=srcfile, fileformat=ESMF_FILEFORMAT_ESMFMESH, &
-                         nodalDistgrid=distgrid1, rc=localrc)
+  srcMesh=ESMF_MeshCreate(filename=srcfile, fileformat=ESMF_FILEFORMAT_ESMFMESH, &
+                          nodalDistgrid=distgrid1, rc=localrc)
   if (localrc /=ESMF_SUCCESS) then
     rc=ESMF_FAILURE
     return
   endif
+
+!   call ESMF_MeshGet(srcMesh, nodalDistgrid=distgrid1, rc=localrc)
+!   if (localrc /=ESMF_SUCCESS) then
+!     rc=ESMF_FAILURE
+!     return
+!   endif
+! 
+!   allocate(ec(4))
+!   call ESMF_DistGridGet(distgrid1, elementCountPDe=ec, rc=localrc)
+!   if (localrc /=ESMF_SUCCESS) then
+!     rc=ESMF_FAILURE
+!     return
+!   endif
+! 
+!   allocate(sil(ec(localPet+1)))
+!   call ESMF_DistGridGet(distgrid1, 0, seqIndexList=sil, rc=localrc)
+!   if (localrc /=ESMF_SUCCESS) then
+!     rc=ESMF_FAILURE
+!     return
+!   endif
+! 
+! print *, localPet, "# distgrid actual minI = ", minval(sil), " maxI = ", maxval(sil)
 
 #ifdef profile_mesh_create
   call ESMF_VMLogMemInfo("after "//NM//" create")
@@ -199,15 +238,17 @@ print *, localPet, "# distgrid2 minI = ", minI, " maxI = ", maxI
 #endif
 
 
+#define profile_mesh_redist
 #ifdef profile_mesh_redist
   call ESMF_TraceRegionEnter(NM//" Redist")
   call ESMF_VMLogMemInfo("before "//NM//" redist")
 
-  call ESMF_MeshCreate(srcMesh, nodalDistgrid=distgrid2, rc=localrc)
+  redistMesh = ESMF_MeshCreate(srcMesh, nodalDistgrid=distgrid2, rc=localrc)
   if (localrc /=ESMF_SUCCESS) then
     rc=ESMF_FAILURE
     return
   endif
+
 
   call ESMF_VMLogMemInfo("after "//NM//" redist")
   call ESMF_TraceRegionExit(NM//" Redist")
