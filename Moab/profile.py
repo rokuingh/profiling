07598be4f816acb2c0fe751960ee3c043ea1e0f8
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-07-07 16:11:26 -0700

    clean up

--- Moab/profile.py
+++ Moab/profile.py
@@ -5,91 +5,12 @@
 # Ryan O'Kuinghttons
 # July 1, 2020
 # profile.py
-# Usage: python profile.py -np <num_cores> -runs <num_runs> -testcase  <testcase|[create,dual,grid2mesh,redist,regrid-bilinear,regrid-conservative,rendezvous]> --esmfmkfile=<ESMFMKFILE> --execdir=<EXECDIR>
-#
+
 import sys, os
 import numpy as np
 import argparse
 import subprocess
 from time import localtime, strftime, time
-
-
-# class Workflow():
-#     from collections import namedtuple
-#     import itertools
-# 
-#     def __init__(self, *args, **kwds):
-#         self.configurations = configurations
-#         # self.configurations = dict(
-#         #     np=[4608],
-#         #     runs=[10],
-#         #     testcase=[create, dual, grid2mesh, redist, regrid, renezvous],
-#         # )
-# 
-#         # testcases = self.iter_product_keywords(keywords)
-#         # fail = []
-#         # for a in testcases:
-#         #     try:
-#         #         grid = Grid(np.array([12, 12]),
-#         #                     pole_kind=np.array(a.pole_kind),
-#         #                     num_peri_dims=a.periodic[0],
-#         #                     periodic_dim=a.periodic[1],
-#         #                     pole_dim=a.periodic[2],
-#         #                     coord_sys=a.coord_sys,
-#         #                     coord_typekind=a.typekind,
-#         #                     staggerloc=a.staggerloc)
-#         #         grid.add_item(GridItem.MASK)
-#         #         grid.add_item(GridItem.AREA)
-#         #         grid2 = grid[2:10, 4:7]
-#         #         self.examine_grid_attributes(grid)
-#         #         self.examine_grid_attributes(grid2)
-#         #         grid.destroy()
-#         #         grid2.destroy()
-#         #     except:
-#         #         fail += a
-# 
-#     if len(fail) > 0:
-#         raise ValueError(
-#             "The following combinations of Grid parameters failed to create a proper Grid: " + str(fail))
-# 
-# 
-# 
-#     def itr_row(key, sequence):
-#         for element in sequence:
-#             yield ({key: element})
-# 
-#     def iter_product_keywords(keywords, as_namedtuple=True):
-#         if as_namedtuple:
-#             yld_tuple = namedtuple('ITesterKeywords', keywords.keys())
-# 
-#         iterators = [itr_row(ki, vi) for ki, vi in keywords.items()]
-#         for dictionaries in itertools.product(*iterators):
-#             yld = {}
-#             for dictionary in dictionaries:
-#                 yld.update(dictionary)
-#             if as_namedtuple:
-#                 yld = yld_tuple(**yld)
-#             yield yld
-# 
-#     def submit_batch_job(self, cmd):
-#         p = subprocess.Popen(cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-#         O = ''
-#         E = ''
-#         last = time()
-#         while p.poll() is None:
-#             if time() - last > 5:
-#                 print('Process is still running')
-#                 last = time()
-#             tmp = p.stdout.read(1)
-#             if tmp:
-#                 O += tmp
-#             tmp = p.stderr.read(1)
-#             if tmp:
-#                 E += tmp
-#         ret = p.poll(), O+p.stdout.read(), E+p.stderr.read() # Catch remaining output
-#         p.stdout.close() # Always close your file handles, or your OS might be pissed
-#         p.stderr.close()
-#         return ret
 
 def parseArguments():
     # Create argument parser
@@ -207,8 +128,6 @@ if __name__ == '__main__':
         print ("Results are in the following files:\n", timingfile, "\n", memoryfile)
     except:
         raise RuntimeError("Error collecting the test results.")
-
-# !! separate memory files into blocks for rss, hwm, tas
 
 # # 4 report: manual, scp, copy and paste the files into drive spreadsheet and make the appropriate graphs
 # 
