d19a9a4c8db029ababd7dc8051aa607eef67fc96
Ryan.O'Kuinghttons <ryan.okuinghttons@noaa.gov>
2020-07-28 14:29:37 -0700

    add more ESMF environment variables

--- ESMF_ProfileMBMesh/config/Cheyenne.py
+++ ESMF_ProfileMBMesh/config/Cheyenne.py
@@ -6,12 +6,15 @@ SRCDIR="/glade/work/rokuingh/sandbox/profiling/ESMF_ProfileMBMesh"
 procs=(36, 72, 144, 288, 576, 1152, 2304, 4608)
 
 esmf_env = dict(ESMF_OS = "Linux",
-               ESMF_COMPILER = "intel",
-               ESMF_COMM = "mpt",
-               ESMF_NETCDF = "split",
-               ESMF_NETCDF_INCLUDE="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/include",
-               ESMF_NETCDF_LIBPATH="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/lib",
-               ESMF_BUILD_NP=36)
+                ESMF_COMPILER = "intel",
+                ESMF_COMM = "mpt",
+                ESMF_NETCDF = "split",
+                ESMF_NETCDF_INCLUDE="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/include",
+                ESMF_NETCDF_LIBPATH="/glade/u/apps/ch/opt/netcdf/4.7.1/intel/18.0.5/lib",
+                ESMF_BOPT="O",
+                ESMF_OPTLEVEL=2,
+                ESMF_ABI=64,
+                ESMF_BUILD_NP=36)
 
 testcase_args = dict(
     create = dict(GRID1 = os.path.join(SRCDIR,"data", "ll1x2e4deg10e7node.esmf.nc"),
--- ESMF_ProfileMBMesh/config/Linux.py
+++ ESMF_ProfileMBMesh/config/Linux.py
@@ -6,12 +6,15 @@ SRCDIR="/home/ryan/Dropbox/sandbox/profiling/ESMF_ProfileMBMesh"
 procs=(1, 2, 4, 8)
 
 esmf_env = dict(ESMF_OS = "Linux",
-               ESMF_COMPILER = "gfortran",
-               ESMF_COMM = "openmpi",
-               ESMF_NETCDF = "split",
-               ESMF_NETCDF_INCLUDE="/usr/local/include",
-               ESMF_NETCDF_LIBPATH="/usr/local/lib",
-               ESMF_BUILD_NP=6)
+                ESMF_COMPILER = "gfortran",
+                ESMF_COMM = "openmpi",
+                ESMF_NETCDF = "split",
+                ESMF_NETCDF_INCLUDE="/usr/local/include",
+                ESMF_NETCDF_LIBPATH="/usr/local/lib",
+                ESMF_BOPT="O",
+                ESMF_OPTLEVEL=2,
+                ESMF_ABI=64,
+                ESMF_BUILD_NP=6)
 
 testcase_args = dict(
     create = dict(GRID1 = os.path.join(SRCDIR,"data", "ll1deg.esmf.nc"),
